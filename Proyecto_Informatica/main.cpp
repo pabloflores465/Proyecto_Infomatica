bb8ea2ddb966f45baadb42bb56bcf001ca65c73a
josalonzo <josalonzo.gt@gmail.com>
2022-10-03 13:59:35 -0600

    Correcion Modulos de la farmacia

--- Proyecto_Informatica/main.cpp
+++ Proyecto_Informatica/main.cpp
@@ -3,37 +3,48 @@
 using namespace std;
 int main() {
     //Arrelos para la descripcion de los farmacos
-    string nombrefarmacos[10] {"ibuprofeno 100mg", "Nauseol 50mg", "PEPTO-BISMOL Cereza 236ml","VICK BEBE BALSAMO 50 GRS", "TABCIN GRIPE Y TOS LIQUI-GELS", "VITAMINA C 500 MG", "AZITROMICINA 500MG", "ASPIRINA ADULTOS 500 MG"," BRONCUROL","DOLO TRIVIPLEX INYECTADO" };
-    string descripcionfarmacos [10] {"Antiinflamatorio","Medicamento para eliminar la naúseas, mareos y vómitos", "Medicamento usado para el tratamiento de la indigestión, el malestar estomacal, la diarrea","Descongestion nasal", "Medicamento para aliviar la tos y la gripe", "Fortalece las defensas del organismo y ayuda a la cicatrizacion", "Antibiotico de amplio espectro", "Medicamento para tratar el dolor, la fiebre y la inflamación", "Medicamento para aliviar la tos y el resfriado", "Medicamento para aliviar dolores neuriticos"};
-    string tipofarmaco [10] {"tabletas","Bebible","Bebible","Gel","tabletas", "tabletas", "tabletas", "tabletas","comprimidos", "inyeccion"};
-    string cantidadfarmaco [10] {"100 tabletas", "2 ampollas","", "", "12 tabletas", "20 tabletas", "12 tabletas", "100 tabletas", "100 comprimidos", "2 ampollas",  };
-    float preciofarmaco [10] {186.95, 26.00, 64.99, 33.02, 26.95, 28.20, 559.98, 96.76, 104.75, 50.38   };
-    int existenciasfarmacos [10] {20,20,20,20,20,20,20,20,20,20};
-    do{
+    string nombrefarmacos[10]{"ibuprofeno 100mg", "Nauseol 50mg", "PEPTO-BISMOL Cereza 236ml",
+                              "VICK BEBE BALSAMO 50 GRS", "TABCIN GRIPE Y TOS LIQUI-GELS", "VITAMINA C 500 MG",
+                              "AZITROMICINA 500MG", "ASPIRINA ADULTOS 500 MG", " BRONCUROL",
+                              "DOLO TRIVIPLEX INYECTADO"};
+    string descripcionfarmacos[10]{"Antiinflamatorio", "Medicamento para eliminar la naúseas, mareos y vómitos",
+                                   "Medicamento usado para el tratamiento de la indigestión, el malestar estomacal, la diarrea",
+                                   "Descongestion nasal", "Medicamento para aliviar la tos y la gripe",
+                                   "Fortalece las defensas del organismo y ayuda a la cicatrizacion",
+                                   "Antibiotico de amplio espectro",
+                                   "Medicamento para tratar el dolor, la fiebre y la inflamación",
+                                   "Medicamento para aliviar la tos y el resfriado",
+                                   "Medicamento para aliviar dolores neuriticos"};
+    string tipofarmaco[10]{"tabletas", "Bebible", "Bebible", "Gel", "tabletas", "tabletas", "tabletas", "tabletas",
+                           "comprimidos", "inyeccion"};
+    string cantidadfarmaco[10]{"100 tabletas", "2 ampollas", "", "", "12 tabletas", "20 tabletas", "12 tabletas",
+                               "100 tabletas", "100 comprimidos", "2 ampollas",};
+    float preciofarmaco[10]{186.95, 26.00, 64.99, 33.02, 26.95, 28.20, 559.98, 96.76, 104.75, 50.38};
+    int existenciasfarmacos[10]{20, 20, 20, 20, 20, 20, 20, 20, 20, 20};
     int modulos;
-    cout<<"Ingrese al modulo que quiere ingresar\n";
-    cout<<"Para modulo de farmacia presione 1\n";
-    cout<<"Para modulo de seguros presione 2\n";
-    cout<<"Para modulo de reportes presione 3\n";
-    cin>>modulos;
-    switch(modulos)
-    {
-        case 1:
-        {
+    do {
+        cout << "Ingrese al modulo que quiere ingresar\n";
+        cout << "Para modulo de farmacia presione 1\n";
+        cout << "Para modulo de seguros presione 2\n";
+        cout << "Para modulo de reportes presione 3\n";
+        cout << "Ingrese el numero 3 para finalizar el proceso";
+        cin >> modulos;
+        switch (modulos) {
+            case 1: {
 
-        }
-        case 2:
-        {
+            }
+            case 2: {
 
-        }
-        case 3:
-        {
+            }
+            case 3: {
 
+            }
+            default: {
+                cout << "Error ingrese un numero valido";
+            }
         }
+
     }
-    }
-
-
-
+    while (modulos != 3);
     return 0;
 }
